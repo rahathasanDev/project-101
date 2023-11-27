121198e8029746ad38bc96048be36d712f85d538
Rahat Hasan <devrahat.hasan@gmail.com>
2023-11-27 17:14:21 +0600

    added navbar route

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+      <a href="/">Home</a>
+      <a href="">Messeages</a>
+      <a href="">Menu</a>
+      <a href="">About</a>
+      <a href="">contact us</a>
+    </div>
+  );
+};
+
+export default Navbar;
